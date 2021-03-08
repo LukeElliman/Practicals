15728fb73936c0579bc74ae46af4dfbaf142742a
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-08 23:17:41 +1000

    Add function for amount of scores

--- Prac_03/scores.py
+++ Prac_03/scores.py
@@ -8,28 +8,40 @@ import random
 
 def main():
     """Contains the main program"""
-    score = float(input("Enter score: "))
-    print(result_determine(score))
-    score = random_score()
-    print("{0} is {1}".format(score, result_determine(score)))
+    number_of_scores = score_choice_amount()
+    print(number_of_scores)
 
 
-def result_determine(score):
-    """Determines the users result based on score"""
-    if score < 0 or score > 100:
-        return "Invalid score"
-    elif score >= 90:
-        return "Excellent"
-    elif 90 > score >= 50:
-        return "Passable"
-    else:
-        return "Bad"
+def score_choice_amount():
+    valid_number = False
+    while not valid_number:
+        try:
+            value = int(input("How many scores do you want? "))
+            if value <= 0:
+                print("The value must be above 0")
+            else:
+                valid_number = True
+        except ValueError:
+            print("You must input an integer")
+    return value
 
 
-def random_score():
-    """Generates a random value between 0 to 100"""
-    random_value = random.randint(0, 100)
-    return random_value
+# def result_determine(score):
+#     """Determines the users result based on score"""
+#     if score < 0 or score > 100:
+#         return "Invalid score"
+#     elif score >= 90:
+#         return "Excellent"
+#     elif 90 > score >= 50:
+#         return "Passable"
+#     else:
+#         return "Bad"
+#
+#
+# def random_score():
+#     """Generates a random value between 0 to 100"""
+#     random_value = random.randint(0, 100)
+#     return random_value
 
 
 main()
