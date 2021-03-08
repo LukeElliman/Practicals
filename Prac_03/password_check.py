64c4da6946dcee6b31016d4ef230e38611631dc6
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-08 23:16:04 +1000

    Fix password_length_verifier()

--- Prac_03/password_check.py
+++ Prac_03/password_check.py
@@ -4,21 +4,20 @@ Luke Elliman"""
 def main():
     """Stores the main part of the program"""
     password = input("Enter the password: ")
-    password_length = int(input("Enter the password length: "))
-    password_length = password_length_verifier(password_length)
+    password_length = password_length_verifier()
     password = user_password_check(password, password_length)
     for i in password:
         print(end="*")
 
 
-def password_length_verifier(length):
+def password_length_verifier():
     """See if the inputted password length is an integer"""
     valid_length = False
     while not valid_length:
         try:
-            while length <= 0:
+            length = int(input("Enter the password length: "))
+            if length <= 0:
                 print("Password length must be above 0")
-                length = int(input("Enter the password length: "))
             else:
                 valid_length = True
         except ValueError:
