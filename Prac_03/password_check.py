961cb5fc15704f0ecbf529fc3e2c71a6f1cdc6da
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-08 17:08:57 +1000

    Initial Commit

--- Prac_03/password_check.py
+++
@@ -1,8 +0,0 @@
-password = input("Enter the password: ")
-password_length = int(input("Enter the password length: "))
-while len(password) < password_length:
-    print("Not long enough")
-    password = input("Enter the password: ")
-
-for i in password:
-    print(end = "*")
