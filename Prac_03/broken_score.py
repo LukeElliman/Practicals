074dc05ea616e4bff6acd49c4fb788d464876c94
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-08 22:34:55 +1000

    Add main()

--- Prac_03/broken_score.py
+++ Prac_03/broken_score.py
@@ -4,14 +4,16 @@ Broken program to determine score status
 """
 
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif 90 > score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    """Contains the main program"""
+    score = float(input("Enter score: "))
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif 90 > score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
 
 
