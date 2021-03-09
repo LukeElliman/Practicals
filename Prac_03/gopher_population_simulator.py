df9c3f5c04ed364ce18143a5c43a65ea430ec41d
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-09 19:32:36 +1000

    Add decrease and increase function

--- Prac_03/gopher_population_simulator.py
+++ Prac_03/gopher_population_simulator.py
@@ -5,8 +5,27 @@ Luke Elliman
 
 import random
 
+INITIAL_POPULATION = 1000
+
 MIN_INCREASE = 0.1
 MAX_INCREASE = 0.2
 
 MIN_DECREASE = 0.05
 MAX_DECREASE = 0.25
+
+
+def main():
+    """Runs the main program"""
+
+
+def increase():
+    increase_amount = random.uniform(MIN_INCREASE, MAX_INCREASE)
+    return increase_amount
+
+
+def decrease():
+    decrease_amount = random.uniform(MIN_DECREASE, MAX_DECREASE)
+    return decrease_amount
+
+
+main()
