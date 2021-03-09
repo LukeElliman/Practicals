10530227b6e879311ad9281ea979d503e995b097
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-09 15:07:12 +1000

    Add main()

--- Prac_03/word_generator_prac_three.py
+++ Prac_03/word_generator_prac_three.py
@@ -10,27 +10,32 @@ import random
 VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
 
-#User input
-word_format = str(input("Enter c's for consonant and v's for vowels: ")).lower()
-word = ""
-for kind in word_format:
-    if kind == "c":
-        word += random.choice(CONSONANTS)
-    else:
-        word += random.choice(VOWELS)
 
-print(word)
-print()
+def main():
+    #User input
+    word_format = str(input("Enter c's for consonant and v's for vowels: ")).lower()
+    word = ""
+    for kind in word_format:
+        if kind == "c":
+            word += random.choice(CONSONANTS)
+        else:
+            word += random.choice(VOWELS)
 
-#Random
-number_of_letters = int(input("How many letters do you want? "))
-letters = "cv"
-word_format = "".join(random.choice(letters) for i in range(number_of_letters))
-word = ""
-for kind in word_format:
-    if kind == "c":
-        word += random.choice(CONSONANTS)
-    else:
-        word += random.choice(VOWELS)
+    print(word)
+    print()
 
-print(word)
+    #Random
+    number_of_letters = int(input("How many letters do you want? "))
+    letters = "cv"
+    word_format = "".join(random.choice(letters) for i in range(number_of_letters))
+    word = ""
+    for kind in word_format:
+        if kind == "c":
+            word += random.choice(CONSONANTS)
+        else:
+            word += random.choice(VOWELS)
+
+    print(word)
+
+
+main()
