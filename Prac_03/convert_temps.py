d5045774d8267a7b2b8d58c033f043474a385eed
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-09 14:57:14 +1000

    Add constants MIN_FAHRENHEIT and MAX_FAHRENHEIT

--- Prac_03/convert_temps.py
+++ Prac_03/convert_temps.py
@@ -6,6 +6,8 @@ Luke Elliman
 import random
 
 MAX_ENTRIES = 20
+MIN_FAHRENHEIT = -200
+MAX_FAHRENHEIT = 200
 
 
 def main():
@@ -24,7 +26,7 @@ def random_fahrenheit_list():
     """Generates a random list of values in a text file"""
     out_file = open("temps_input.txt", "w")
     for i in range(0, MAX_ENTRIES + 1, 1):
-        print(random.uniform(-200, 200), file=out_file)
+        print(random.uniform(MIN_FAHRENHEIT, MAX_FAHRENHEIT), file=out_file)
     out_file.close()
 
 
