f52d3699b934f99a52c9dc20f1a00cf6c5743d09
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-15 21:25:44 +1000

    Initial commit

---
+++ Prac_04/lists_warmup.py
@@ -0,0 +1,11 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+print(numbers[0])   #3
+print(numbers[-1])  #2
+print(numbers[3])   #1
+print(numbers[:-1]) #3, 1, 4, 1, 5, 9
+print(numbers[3:4]) #1
+print(5 in numbers) #true
+print(7 in numbers) #false
+print("3" in numbers) #false
+print(numbers + [6, 5, 3]) #[3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
