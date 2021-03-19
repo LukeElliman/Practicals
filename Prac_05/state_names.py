0e23771dfee9741915cbc24a5e5419a3be7e6f50
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-19 20:33:49 +1000

    Add code to print all states

--- Prac_05/state_names.py
+++ Prac_05/state_names.py
@@ -17,3 +17,6 @@ while state_code != "":
         print("Invalid short state")
     state_code = (input("Enter short state: ")).upper()
 
+for code, state_name in CODE_TO_NAME.items():
+    print("{0:>5} is {1:>5}".format(code, state_name))
+
