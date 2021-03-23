6f38ecf3965badc3cd22b6c0a20b3b01f38dc8c8
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-23 18:05:35 +1000

    Add print amount of word occurrence

--- Prac_05/word_occurrences.py
+++ Prac_05/word_occurrences.py
@@ -1,7 +1,6 @@
 """Count word occurences in a string"""
 
 word_to_count = {}
-print(word_to_count)
 # text = str(input("Text: "))
 text = "this is a collection of words of nice words this is a fun thing it is"
 
@@ -10,3 +9,6 @@ for word in words:
     amount = word_to_count.get(word, 0)
     word_to_count[word] = amount + 1
 
+final_words = list(word_to_count)
+for word in final_words:
+    print("{0} : {1}".format(word, word_to_count[word]))
