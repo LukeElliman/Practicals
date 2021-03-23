56f38a6e8fadb8d22918202c03a7f73defa1f043
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-23 17:47:50 +1000

    Add code to print colour name input to hex

--- Prac_05/hex_colours.py
+++ Prac_05/hex_colours.py
@@ -7,3 +7,15 @@ NAME_TO_CODE = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite
                 "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6", "aquamarine4": "#458b74", "azure1": "#f0ffff"}
 
 print(NAME_TO_CODE)
+
+valid = False
+while not valid:
+    try:
+        colour_name = str(input("Enter a colour: "))
+        if colour_name != "":
+            print("{0} is {1} in hex".format(colour_name, NAME_TO_CODE[colour_name]))
+        else:
+            valid = True
+    except KeyError:
+        print("Not a colour")
+
