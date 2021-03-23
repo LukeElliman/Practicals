da7797bbb74cb50d53d4750f01c268cddd288163
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-23 19:00:10 +1000

    Add code to make dictionary of emails to name

--- Prac_05/emails.py
+++ Prac_05/emails.py
@@ -4,9 +4,19 @@ Email to name
 
 
 def main():
+    """Make dictionary of names and emails"""
+    email_to_name = {}
     email = str(input("Email: "))
-    name = get_name_from_email(email)
-    print(name)
+    while email != "":
+        name = get_name_from_email(email)
+        name_conformation = str(input("Is your name {} (Y/N)".format(name)))
+        if name_conformation.upper() != "Y" and name_conformation != "":
+            name = str(input("Enter your name: "))
+        email_to_name[email] = name
+        email = str(input("Email: "))
+
+    for email, name in email_to_name.items():
+        print("{}  ({})".format(name, email))
 
 
 def get_name_from_email(email: str) -> str:
