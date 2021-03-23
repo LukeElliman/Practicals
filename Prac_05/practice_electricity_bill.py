ae343d84fe76fc6046beaaae4cfbb3fb37bba222
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-23 19:34:56 +1000

    Add dictionary for tariff choice

--- Prac_05/practice_electricity_bill.py
+++ Prac_05/practice_electricity_bill.py
@@ -1,21 +1,19 @@
-TARIFF = {"TARRIFF_11": 0.244618, "TARRIFF_31": 0.136928}
+"""
+Electricity Bill 2.0
+"""
+
+TARIFF = {"11": 0.244618, "31": 0.136928, "45": 0.875391, "7": 0.298463, "57": 0.987654}
 price_per_kWh = None
 
 print("Electricity bill estimator 2.0")
 
-tariff_choice = int(input("Which tariff? 11 or 31: "))
+tariff_choice = str(input("Which tariff 7, 11, 31, 45, 57: "))
+while tariff_choice not in TARIFF:
+    print("Not a valid tariff")
+    tariff_choice = str(input("Which tariff 7, 11, 31, 45, 57: "))
+price_per_kWh = TARIFF[tariff_choice]
 daily_use_kWh = float(input("Enter daily use in kWh: "))
 billing_days = int(input("Enter number of billing days: "))
-
-#Makes sure tariff choice is valid.
-while price_per_kWh is None:
-    if tariff_choice == int(11):
-        price_per_kWh = TARIFF_11
-    elif tariff_choice == int(31):
-        price_per_kWh = TARIFF_31
-    else:
-        print("You did not enter a valid tariff")
-        tariff_choice = int(input("Which tariff? 11 or 31: "))
 
 
 estimated_bill = (price_per_kWh * float(daily_use_kWh) * int(billing_days))
