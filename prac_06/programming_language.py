441e7be5f5e26b85a27dfe4c6fe6d30b1e2c420a
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-30 15:19:40 +1000

    Update __str__

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,8 +1,9 @@
+"""Programming Language class"""
 
 
 class ProgrammingLanguage:
 
-    def __init__(self, name="", typing="", reflection="", year=0):
+    def __init__(self, name, typing, reflection, year):
         """Represent information about a programming language"""
         self.name = name
         self.typing = typing
@@ -11,9 +12,8 @@ class ProgrammingLanguage:
 
     def __str__(self):
         """Return string representation of a ProgrammingLanguage"""
-        return"{}, Typing {}, Reflection {}, Year {}".format(self.name, self.typing,
-                                                             self.reflection,
-                                                             self.year)
+        return"{}, {} Typing, Reflection={}, First appeared in {}"\
+            .format(self.name, self.typing, self.reflection, self.year)
 
     def is_dynamic(self):
         if self.typing == "Dynamic":
