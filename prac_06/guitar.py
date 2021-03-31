bf2dd7bacd6d73004ac43174c26184b6d33ae74f
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-31 14:53:35 +1000

    Initial commit

---
+++ prac_06/guitar.py
@@ -0,0 +1,12 @@
+"""Guitar class"""
+
+
+class Guitar:
+    """Represent a guitar object"""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a car instance"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
