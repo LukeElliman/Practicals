7b708ff98e7ec61f79790fbead8e89734aed3d46
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-04-20 19:13:59 +1000

    Add up down buttons

---
+++ Prac_07/KivyDemos-master/convert_miles_km.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+class MileToKmApp(App):
+    "Kivy app to convert miles to km"
+    def build(self):
+        "Initializes kivy app from kv file"
+        Window.size = (400, 400)
+        self.title = "Mile to Km"
+        self.root = Builder.load_file("convert_miles_km.kv")
+        return self.root
+
+
+MileToKmApp().run()
