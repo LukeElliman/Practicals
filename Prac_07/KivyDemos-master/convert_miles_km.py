489e5e9e0e0a0772c0a14e9e966543a30db9d4b9
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-04-22 19:57:13 +1000

    Fix code so it actually automatically converts

--- Prac_07/KivyDemos-master/convert_miles_km.py
+++ Prac_07/KivyDemos-master/convert_miles_km.py
@@ -21,12 +21,12 @@ class MileToKmApp(App):
         """method to increment by -1 or +1 with up and down"""
         miles = self.convert_to_float(text) + increment
         self.root.ids.mile_input.text = str(miles)
-        self.convert_mile_to_km()
+        self.convert_mile_to_km(miles)
 
-    def convert_mile_to_km(self):
+    def convert_mile_to_km(self, miles):
         """method to convert miles to km"""
-        miles = self.convert_to_float(self.root.ids.mile_input.text)
-        self.output_km = str(miles * MILE_TO_KM_CONVERSION)
+        miles_float = self.convert_to_float(miles)
+        self.output_km = str(miles_float * MILE_TO_KM_CONVERSION)
 
     def convert_to_float(self, text):
         """method to convert the text input in float"""
