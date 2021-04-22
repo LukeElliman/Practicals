99c70249ea67b247723b234fe0d96e936fb2b7d3
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-04-22 19:48:14 +1000

    Add ability to use up and down button

--- Prac_07/KivyDemos-master/convert_miles_km.py
+++ Prac_07/KivyDemos-master/convert_miles_km.py
@@ -5,6 +5,7 @@ from kivy.properties import StringProperty
 
 MILE_TO_KM_CONVERSION = 1.609
 
+
 class MileToKmApp(App):
     """Kivy app to convert miles to km"""""
 
@@ -16,11 +17,19 @@ class MileToKmApp(App):
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
 
-    def handle_press(self):
+    def handle_increment(self, text, increment):
+        """method to increment by -1 or +1 with up and down"""
+        miles = self.convert_to_float(text) + increment
+        self.root.ids.mile_input.text = str(miles)
+        self.convert_mile_to_km()
+
+    def convert_mile_to_km(self):
+        """method to convert miles to km"""
         miles = self.convert_to_float(self.root.ids.mile_input.text)
         self.output_km = str(miles * MILE_TO_KM_CONVERSION)
 
     def convert_to_float(self, text):
+        """method to convert the text input in float"""
         try:
             return float(text)
         except ValueError:
