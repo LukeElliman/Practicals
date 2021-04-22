82866fe7187703423e543fdf3c8ebeb8096b59aa
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-04-22 18:22:43 +1000

    Add code to convert mile to km

--- Prac_07/KivyDemos-master/convert_miles_km.py
+++ Prac_07/KivyDemos-master/convert_miles_km.py
@@ -1,16 +1,33 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.core.window import Window
+from kivy.properties import StringProperty
 
+
+MILE_TO_KM_CONVERSION = 1.609
 
 class MileToKmApp(App):
     """Kivy app to convert miles to km"""""
+
+    output_km = StringProperty()
+
     def build(self):
         """Initializes kivy app from kv file"""
-        Window.size = (400, 400)
         self.title = "Mile to Km"
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
+
+    def handle_press(self):
+        miles = self.convert_to_float(self.root.ids.mile_input.text)
+        self.output_km = str(miles * MILE_TO_KM_CONVERSION)
+
+    def convert_to_float(self, text):
+        try:
+            return float(text)
+        except ValueError:
+            return 0.0
+
+
+
 
 
 MileToKmApp().run()
