28fdf506ec52592ff185d9c2d95111dd792df290
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-04-26 22:57:24 +1000

    Made price_per_km shared value

--- Prac_08/test_taxi.py
+++ Prac_08/test_taxi.py
@@ -4,7 +4,7 @@ from Prac_08.taxi import Taxi
 
 def main():
     """Code to test a taxi"""
-    prius_one = Taxi("Pirus 1", 100, 1.23)
+    prius_one = Taxi("Pirus 1", 100)
     prius_one.drive(40)
     print(prius_one)
     prius_one.start_fare()
