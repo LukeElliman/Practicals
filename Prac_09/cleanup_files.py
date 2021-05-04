96e073fb15191be3236402b97ee51799badd7fde
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-05-04 22:28:37 +1000

    Remove redundant code

---
+++ Prac_09/cleanup_files.py
@@ -0,0 +1,29 @@
+"""
+CP1404/CP5632 Practical
+Demos of various os module examples
+"""
+import os
+
+
+def main():
+    """Process all subdirectories using os.walk()."""
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
+
+        for filename in filenames:
+            full_name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            os.rename(full_name, new_name)
+
+
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    return new_name
+
+
+main()
