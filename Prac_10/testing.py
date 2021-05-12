c6cb975e95d3b5e4e88ec22a141a74eb3c251133
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-05-12 21:26:19 +1000

    Add car tests

--- Prac_10/testing.py
+++ Prac_10/testing.py
@@ -32,7 +32,6 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
     # Hint: "-".join(["yo", "yo"] -> "yo-yo"
 
     # assert test with custom message,
@@ -46,7 +45,10 @@ def run_tests():
     # using the value passed in or the default
     # You should test both of these
     test_car = Car(fuel=10)
+    assert test_car.fuel == 10
 
+    test_car = Car()
+    assert test_car.fuel == 0
 
 run_tests()
 
